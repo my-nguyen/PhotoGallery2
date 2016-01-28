35fef8afe05cb9fd8cc98bd384f3bc15f135b3c1
iceguyvn <nguyen_my@yahoo.com>
2016-01-27 22:43:39 -0800

    display on screen in 3 columns the caption of the photos from Flickr

--- app/src/main/java/com/bignerdranch/android/photogallery2/PhotoGalleryFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery2/PhotoGalleryFragment.java
@@ -5,18 +5,20 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by My on 1/27/2016.
  */
 public class PhotoGalleryFragment extends Fragment {
-   private RecyclerView    mPhotoRecyclerView;
+   private RecyclerView       mPhotoRecyclerView;
+   private List<GalleryItem>  mItems = new ArrayList<>();
    private static final String   TAG = "PhotoGalleryFragment";
 
    public static PhotoGalleryFragment newInstance() {
@@ -38,15 +40,73 @@ public class PhotoGalleryFragment extends Fragment {
       mPhotoRecyclerView = (RecyclerView)view.findViewById(R.id.fragment_photo_gallery_recycler_view);
       // lay out items in a grid of 3 columns
       mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+      // every time a new RecyclerView is created, it's reconfigured with an appropriate adapter
+      setupAdapter();
 
       return view;
    }
 
-   private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+   private void setupAdapter() {
+      // make sure this PhotoGalleryFragment has been attached to an Activity so getActivity() is
+      // not null. the reason is that AsyncTask may trigger callbacks from the background thread
+      // when the Fragment is not attached to an Activity.
+      if (isAdded())
+         // set up an Adapter based on the list of GalleryItem's
+         mPhotoRecyclerView.setAdapter(new PhotoAdapter(mItems));
+   }
+
+   private class PhotoHolder extends RecyclerView.ViewHolder {
+      private TextView  mTitleTextView;
+
+      public PhotoHolder(View view) {
+         super(view);
+         mTitleTextView = (TextView)view;
+      }
+
+      public void bindGalleryItem(GalleryItem item) {
+         mTitleTextView.setText(item.toString());
+      }
+   }
+
+   private class PhotoAdapter extends RecyclerView.Adapter<PhotoHolder> {
+      private List<GalleryItem>  mGalleryItems;
+
+      public PhotoAdapter(List<GalleryItem> galleryItems) {
+         mGalleryItems = galleryItems;
+      }
+
       @Override
-      protected Void doInBackground(Void... params) {
-         new FlickrFetchr().fetchItems();
-         return null;
+      public PhotoHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+         TextView textView = new TextView(getActivity());
+         return new PhotoHolder(textView);
+      }
+
+      @Override
+      public void onBindViewHolder(PhotoHolder holder, int position) {
+         GalleryItem item = mGalleryItems.get(position);
+         holder.bindGalleryItem(item);
+      }
+
+      @Override
+      public int getItemCount() {
+         return mGalleryItems.size();
+      }
+   }
+
+   private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+      @Override
+      // this method is run in the background thread. it returns a List of GalleryItem's from Flickr
+      protected List<GalleryItem> doInBackground(Void... params) {
+         return new FlickrFetchr().fetchItems();
+      }
+
+      @Override
+      // this method is run on the main thread
+      protected void onPostExecute(List<GalleryItem> items) {
+         // save the list of GalleryItem's fetched by doInBackGround()
+         mItems = items;
+         // set up Adapter with this new set of data
+         setupAdapter();
       }
    }
 }
