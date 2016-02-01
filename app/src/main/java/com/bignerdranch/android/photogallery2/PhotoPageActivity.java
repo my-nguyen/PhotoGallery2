692b41ffbe812f27a39eca257a78bf571f1e00ff
iceguyvn <nguyen_my@yahoo.com>
2016-02-01 12:19:11 -0800

    clicking on individual GalleryItem will now launch a WebView fragment with contents from the URL provided

--- app/src/main/java/com/bignerdranch/android/photogallery2/PhotoPageActivity.java
+++ app/src/main/java/com/bignerdranch/android/photogallery2/PhotoPageActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 /**
  * Created by My on 2/1/2016.
  */
+// this class serves as the host for PhotoPageFragment, the WebView fragment
 public class PhotoPageActivity extends SingleFragmentActivity {
    public static Intent newIntent(Context context, Uri photoPageUri) {
       Intent intent = new Intent(context, PhotoPageActivity.class);
--- app/src/main/java/com/bignerdranch/android/photogallery2/PhotoPageFragment.java
+++ app/src/main/java/com/bignerdranch/android/photogallery2/PhotoPageFragment.java
@@ -1,16 +1,18 @@
 package com.bignerdranch.android.photogallery2;
 
+import android.annotation.SuppressLint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by My on 2/1/2016.
  */
-// this class will display the WebView
+// this class serves to display the WebView
 public class PhotoPageFragment extends VisibleFragment {
    private static final String   ARG_URI = "photo_page_url";
    private Uri       mUri;
@@ -31,9 +33,24 @@ public class PhotoPageFragment extends VisibleFragment {
    }
 
    @Override
+   @SuppressLint("SetJavaScriptEnabled")
    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
       View view = inflater.inflate(R.layout.fragment_photo_page, container, false);
       mWebView = (WebView)view.findViewById(R.id.fragment_photo_page_web_view);
+      // turn on Javascript, as required by Flickr
+      mWebView.getSettings().setJavaScriptEnabled(true);
+      mWebView.setWebViewClient(new WebViewClient() {
+         // this method determines what will happen when a new URL is loaded in the WebView.
+         // the default implementation fires an implicit intent with the URL. Flickr would redirect
+         // you to the mobile version of the website. with the default WebViewClient, you are
+         // immediately sent to the user’s default web browser.
+         // to load it in this WebView, just override the default implementation and return false.
+         public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+            return false;
+         }
+      });
+      // load the URI after configuring the WebView
+      mWebView.loadUrl(mUri.toString());
       return view;
    }
 }
